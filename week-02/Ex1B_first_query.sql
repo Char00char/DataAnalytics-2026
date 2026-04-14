b51414e2ebb486154b4e16738612fc2b62abe0fa
charles palacios <cpalacios@my.yearupunited.org>
2026-04-14 09:05:55 -0400

    Updating Week 2 1B docs

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,8 @@
 /*
 Sakila is a database used to collect information about movie rentals at a movie rental store
 */
+
+USE sakila; 
+SELECT * FROM actor; -- Retrived 200 records
+
+SELECT * FROM film; -- Retrieced 1000 records
