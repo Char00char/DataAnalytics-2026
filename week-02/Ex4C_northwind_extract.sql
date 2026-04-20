061e0387e1788396679ee6396036ace4ac93f689
Charles Palacios <cpprlsa@gmail.com>
2026-04-20 11:02:21 -0400

    Added Ex4C docs

---
+++ week-02/Ex4C_northwind_extract.sql
@@ -0,0 +1,10 @@
+-- Prodcuts holds the items that Northwind sellls
+-- Categories hold the types/categories of items
+SELECT * FROM employees; 
+-- Margaret Peacock is the Northwind employee whose name makes it look like she’s a bird
+SELECT * FROM products;
+-- There at 77 records, 
+SELECT * FROM categories;
+-- The CategoryID for Seafood is 8
+SELECT OrderID, OrderDate, ShipName, ShipCountry FROM orders;
+
