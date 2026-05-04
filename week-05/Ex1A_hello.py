534cf63b79c2f073ad2560709a4a4edc49e16663
Charles Palacios <cpprlsa@gmail.com>
2026-05-04 14:02:23 -0400

    Updated 1A docs

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,19 @@
 print("Hello, World!")
+
+message = "Hello, World!"
+print(message)
+
+# "Hello, World!" prints twice because we have two print statements. The first one prints the string directly, while the second one prints the value of the variable 'message', which contains the same string.
+
+#Display dollars and cents  
+dollars = 3
+cents = .50
+print(dollars + cents)
+# The output of the above code will be 3.5 because we are adding the integer value of dollars (3) and the float value of cents (0.50). The result is a float value representing the total amount in dollars and cents.
+
+cents = cents + .25
+print(dollars + cents)
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + ' ' + c_str)
