22a616620f691f203f022fcce5418081e701e383
Charles Palacios <cpprlsa@gmail.com>
2026-05-11 19:18:00 -0400

    Added Net Worth script

---
+++ week-05/Ex2A_MathScripts/net_worth.py
@@ -0,0 +1,17 @@
+# Some assests include:
+cash = 400
+savings = 9000
+car = 13000
+investments = 5000
+
+#Some debts include:
+credit_card = 1750
+student_loan = 10000
+
+total_assets = cash + savings + car + investments
+total_debts = credit_card + student_loan
+net_worth = total_assets - total_debts
+
+print("Your total assets are", total_assets)
+print("Your total debts are", total_debts)
+print("Your net worth is", net_worth)
