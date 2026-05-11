d14538968f86449aa6265252e3193353feb66e77
Charles Palacios <cpprlsa@gmail.com>
2026-05-11 19:31:26 -0400

    Added Area of Rectangle calculations

---
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,11 @@
+#Calculate the area of a rectangle using my birth month and day
+side_a = 8
+side_b = 1
+
+#Calculation of rectangle area
+area = side_a * side_b
+
+#Print the result
+print("Side A is", side_a)
+print("Side B is", side_b)
+print("The area of the rectangle is", area)
