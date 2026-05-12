97af82a0ac004c5bf9ddf4c922debf3a7caa3ed6
Charles Palacios <cpprlsa@gmail.com>
2026-05-11 20:07:43 -0400

    Updated Rectangle script with input script

--- week-05/Ex2A_MathScripts/area_of_rectangle.py
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -9,3 +9,15 @@ area = side_a * side_b
 print("Side A is", side_a)
 print("Side B is", side_b)
 print("The area of the rectangle is", area)
+
+#Input script to calculate the area of a rectangle
+side_a = float(input("Enter the length of side A: "))
+side_b = float(input("Enter the length of side B: "))
+
+#Calculate the area
+area = side_a * side_b
+
+#Print the result
+print("Side A is", side_a)
+print("Side B is", side_b)
+print("The area of the rectangle is", area)
